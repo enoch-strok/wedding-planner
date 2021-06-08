e42a144d87af7f1f86370dcfcfc9d5a2f68e1d48
Enoch Strok <50689197+enoch-strok@users.noreply.github.com>
2021-06-07 22:15:22 -0700

    updated views

--- src/App.js
+++ src/App.js
@@ -1,14 +1,11 @@
 
 import './App.css';
-import BootStrap from './components/BootStrap';
-import Header from './components/Header';
-import Main from './components/Main';
-import LandingPage from "./components/LandingPage/LandingPage";
+import Dashboard from './views/Dashboard';
 
 function App() {
   return (
     <div className="App">
-      <LandingPage/>
+      <Dashboard/>
     </div>
   );
 }
---
+++ src/views/Dashboard.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import LandingPage from "../components/LandingPage/LandingPage";
+
+const Dashboard = props => {
+
+    return (
+        <> 
+            <LandingPage/>
+        </>
+    )
+}
+
+export default Dashboard;
+
