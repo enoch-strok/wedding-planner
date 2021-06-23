0278e452b7316b11806ac7c6ac29c4a51e90c88d
Enoch <enochstrok@outlook.com>
2021-06-23 02:19:01 -0700

    added completed and alerts feature to RSVP form

--- src/components/LandingPage/Sections/RSVP.jsx
+++ src/components/LandingPage/Sections/RSVP.jsx
@@ -1,8 +1,10 @@
 import React, {useEffect, useState} from 'react';
 import axios from 'axios';
+import ReactDom from 'react-dom';
+import Popup from 'react-popup';
 // @material-ui/core components
 import { makeStyles } from "@material-ui/core/styles";
-
+import { Alert } from 'react-bootstrap';
 // @material-ui/icons
 
 // core components
@@ -10,7 +12,6 @@ import GridContainer from "components/Grid/GridContainer.js";
 import GridItem from "components/Grid/GridItem.js";
 import CustomInput from "components/CustomInput/CustomInput.js";
 import Button from "components/CustomButtons/Button.js";
-
 import styles from "assets/jss/material-kit-react/views/landingPageSections/workStyle.js";
 
 const useStyles = makeStyles(styles);
@@ -21,7 +22,8 @@ const RSVP = (props) => {
   useEffect(() => {
     console.log(inputList)
   }, [inputList])
-  const {guestName, guestPhone, numberOfGuests} = inputList;
+
+  const [alerts, setAlerts] = useState(0);
 
   const [trigger, setTrigger] = useState(0);
   const triggerCount = () => setTrigger(currCount => currCount + 1);
@@ -46,15 +48,23 @@ const RSVP = (props) => {
         })
         .then(res => {
             console.log("Server Response: ",res);
-            setTrigger(trigger + 1);
+            triggerCount();
+            setAlerts(res.status);
         })
         .catch(err => console.log("Server Error Message: ",err));
-  }
+    
+      document.getElementById("rsvp_form").reset();
 
+  }
+  useEffect(() => {
+    console.log('alerts2:',alerts)
+  }, [alerts])
 
   const classes = useStyles();
   return (
     <div className={classes.section}>
+      
+      {alerts === 0 &&
       <GridContainer justify="center">
         <GridItem cs={12} sm={12} md={8}>
           <h2>RSVP - Reserve Your Spot</h2>
@@ -64,7 +74,7 @@ const RSVP = (props) => {
             Please let us know if you are planning to make it. We understand that life happens but it helps us to let others who also want to make it take your free spot if you are not able to.
           </h5>
           <br></br>
-          <form onSubmit={submitForm}>
+          <form id="rsvp_form" onSubmit={submitForm}>
             
               <GridItem xs={12} sm={12} md={12}>
                 <CustomInput
@@ -101,34 +111,37 @@ const RSVP = (props) => {
               <br></br>
               <br></br>
 
-              {/* <CustomInput
-                labelText="Your Message"
-                id="message"
-                formControlProps={{
-                  fullWidth: true,
-                  className: classes.textArea,
-                }}
-                inputProps={{
-                  multiline: true,
-                  rows: 5,
-                }}
-              /> */}
-
-              
-            
-              {/* <GridContainer> */}
-                {/* <GridItem xs={12} sm={12} md={6}> */}
+              <GridContainer>
+                <GridItem xs={12} sm={12} md={6}>
                   <Button color="success"
                   type="submit"
                   >I Will Be There</Button>
-                {/* </GridItem> */}
-                {/* <GridItem xs={12} sm={12} md={6}> */}
+                </GridItem>
+                <GridItem xs={12} sm={12} md={6}>
                   <Button color="danger">I Cannot Make It</Button>
-                {/* </GridItem> */}
-              {/* </GridContainer> */}
+                </GridItem>
+              </GridContainer>
           </form>
         </GridItem>
       </GridContainer>
+      }
+      {alerts === 200 &&
+                <Alert variant="success">
+                  <Alert.Heading>
+                    Thank you for letting us know!
+                  </Alert.Heading>
+                  <p>Your response has been saved.</p>
+                </Alert>
+      }
+      {alerts != 200 && alerts != 0 &&
+                <Alert variant="danger">
+                  <Alert.Heading>
+                    There was an error!
+                  </Alert.Heading>
+                  <p>Please let Enoch know. 206-356-0204</p>
+                </Alert>
+      }
+
     </div>
   );
 }
